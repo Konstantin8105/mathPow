3f546b30ee3357ecfe645f2eeb934b20ca085a80
Konstantin8105 <konstantin8105@gmail.com>
2019-01-02 21:46:14 +0300

    optimization sqrt

--- pow_test.go
+++ pow_test.go
@@ -13,6 +13,7 @@ var fpow = []struct {
 	{pow, "pow"},
 	{pow_des1, "pow_des1"},
 	{pow_des2, "pow_des2"},
+	{pow_sq1, "pow_sq1"},
 	{math.Pow, "math.Pow"},
 }
 
@@ -20,7 +21,19 @@ var tcs = []struct {
 	name string
 	x, y float64
 }{
-	{"SIMPLE", math.Pi, 2},
+	{"SIMPLE", math.Pi, 7.0},
+	{"SQRT2+", math.Pi, 1.0 / 2.0},
+	{"SQRT2-", math.Pi, -1.0 / 2.0},
+	{"SQRT3", math.Pi, -1.0 / 3.0},
+	{"SQRT8+", math.Pi, 1.0 / 8.0},
+	{"SQRT8-", math.Pi, -1.0 / 8.0},
+	{"SQRT32+", math.Pi, 1.0 / 32.0},
+	{"SQRT32-", math.Pi, -1.0 / 32.0},
+	{"SQRT64", math.Pi, -1.0 / 64.0},
+	{"SQRT128-", math.Pi, -1.0 / 128.0},
+	{"SQRT128+", math.Pi, 1.0 / 128.0},
+	{"SQRT1024-", math.Pi, -1.0 / 1024.0},
+	{"SQRT1024+", math.Pi, 1.0 / 1024.0},
 	{"BASE", 1.2345e-5, -2.5},
 
 	{"PI", math.Pi, 126},
@@ -39,6 +52,19 @@ var tcs = []struct {
 }
 
 func Test(t *testing.T) {
+	tcs := tcs
+	for i := -200; i <= 200; i++ {
+		tcs = append(tcs, struct {
+			name string
+			x, y float64
+		}{"e", math.Pi, float64(i)})
+	}
+	for i := -200; i <= 200; i++ {
+		tcs = append(tcs, struct {
+			name string
+			x, y float64
+		}{"e", math.Pi, float64(i) + 0.1})
+	}
 	for _, p := range fpow {
 		for _, tc := range tcs {
 			t.Run(fmt.Sprintf("%s/%8s/x:%.2e/y:%.1f", tc.name, p.name, tc.x, tc.y), func(t *testing.T) {
