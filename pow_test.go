c63216c70620e633089dc3fbdc6255ab4f18dc16
Konstantin8105 <konstantin8105@gmail.com>
2019-01-02 19:11:02 +0300

    add new design 1

--- pow_test.go
+++ pow_test.go
@@ -11,37 +11,42 @@ var fpow = []struct {
 	name string
 }{
 	{pow, "pow"},
+	{pow_des1, "pow_des1"},
 	{math.Pow, "math.Pow"},
 }
 
 var tcs = []struct {
+	name string
 	x, y float64
 }{
-	{math.Pi, 126},
-	{math.Pi, 125},
-	{math.Pi, 2},
-	{math.Pi, -2},
-	{math.Pi, -125},
-	{math.Pi, -126},
+	{"SIMPLE", math.Pi, 2},
+	{"BASE", 1.2345e-5, -2.5},
 
-	{1.2345e-5, 126},
-	{1.2345e-5, 125},
-	{1.2345e-5, 2},
-	{1.2345e-5, -2},
-	{1.2345e-5, -125},
-	{1.2345e-5, -126},
+	{"PI", math.Pi, 126},
+	{"PI", math.Pi, 125},
+	{"PI", math.Pi, 2},
+	{"PI", math.Pi, -2},
+	{"PI", math.Pi, -125},
+	{"PI", math.Pi, -126},
+
+	{"SMALL", 1.2345e-5, 126},
+	{"SMALL", 1.2345e-5, 125},
+	{"SMALL", 1.2345e-5, 2},
+	{"SMALL", 1.2345e-5, -2},
+	{"SMALL", 1.2345e-5, -125},
+	{"SMALL", 1.2345e-5, -126},
 }
 
 func Test(t *testing.T) {
 	for _, p := range fpow {
-		for i := range tcs {
-			t.Run(fmt.Sprintf("%d", i), func(t *testing.T) {
-				x := tcs[i].x
-				y := tcs[i].y
+		for _, tc := range tcs {
+			t.Run(fmt.Sprintf("%s/%8s/x:%.2e/y:%.1f", tc.name, p.name, tc.x, tc.y), func(t *testing.T) {
+				x := tc.x
+				y := tc.y
 				expect := math.Pow(x, y)
 				a := p.f(x, y)
-				if expect != a {
-					t.Errorf("expect: %.14e. actual: %.14e", expect, a)
+				if math.Abs((expect-a)/a) > 1e-15 {
+					t.Errorf("\nexpect: %.14e\nactual: %.14e\ndiff  : %.14e", expect, a, (expect-a)/a)
 				}
 			})
 		}
@@ -53,7 +58,7 @@ var Nil float64
 func Benchmark(b *testing.B) {
 	for _, p := range fpow {
 		for _, tc := range tcs {
-			b.Run(fmt.Sprintf("%8s/x:%.2e/y:%.1f", p.name, tc.x, tc.y), func(b *testing.B) {
+			b.Run(fmt.Sprintf("%s/%8s/x:%.2e/y:%.1f", tc.name, p.name, tc.x, tc.y), func(b *testing.B) {
 				x := tc.x
 				y := tc.y
 				for i := 0; i < b.N; i++ {
